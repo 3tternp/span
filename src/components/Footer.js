32430b882609be42e316b5836360efc1e9ae2090
astra <26215963+3tternp@users.noreply.github.com>
2024-05-09 16:48:46 +0545

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/prem-basnet524/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.facebook.com/profile.php?id=100051820911738"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/astra.x3/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
           </Col>
