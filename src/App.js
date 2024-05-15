959a9a9876a18bf714f0016313217a94b49d5170
astra <26215963+3tternp@users.noreply.github.com>
2024-05-15 12:25:54 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.png';
+import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Footer } from "./components/Footer";
+import Footer from "./components/Footer.js";
 
 function App() {
   return (
