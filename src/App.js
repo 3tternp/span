e56d611caa092f81ac75947d40d356b9c312a8a2
astra <26215963+3tternp@users.noreply.github.com>
2024-05-15 12:37:44 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import Footer from "./components/Footer.js";
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
-      <Footer />
     </div>
   );
 }
