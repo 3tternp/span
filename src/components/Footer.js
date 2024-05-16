e0f899523d9208db299bb82851090866b60cf1d5
astra <26215963+3tternp@users.noreply.github.com>
2024-05-16 11:48:37 +0545

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,67 +1,32 @@
-import {
-    Box,
-    FooterContainer,
-    Row,
-    Column,
-    FooterLink,
-    Heading,
-} from "./FooterStyles";
+import React from 'react';
+import './Footer.css'; // Import your CSS file for styling
 
-export const Footer = () => {
-    return (
-      <Box>
-      <Footer>
-                 <Row>
-   
-                     <Column>
-                         <Heading>Social Media</Heading>
-                         <FooterLink href="https://www.facebook.com/profile.php?id=100051820911738">
-                             <i className="fab fa-facebook-f">
-                                 <span
-                                     style={{
-                                         marginLeft: "10px",
-                                     }}
-                                 >
-                                     Facebook
-                                 </span>
-                             </i>
-                         </FooterLink>
-                         <FooterLink href="https://www.instagram.com/astra.x3/">
-                             <i className="fab fa-instagram">
-                                 <span
-                                     style={{
-                                         marginLeft: "10px",
-                                     }}
-                                 >
-                                     Instagram
-                                 </span>
-                             </i>
-                         </FooterLink>
-                         <FooterLink href="https://twitter.com/astra524">
-                             <i className="fab fa-twitter">
-                                 <span
-                                     style={{
-                                         marginLeft: "10px",
-                                     }}
-                                 >
-                                     Twitter
-                                 </span>
-                             </i>
-                         </FooterLink>
-                         <FooterLink href="https://www.youtube.com/channel/UCOx8z4HZ4MEf-Wkx_f4JP9Q">
-                             <i className="fab fa-youtube">
-                                 <span
-                                     style={{
-                                         marginLeft: "10px",
-                                     }}
-                                 >
-                                     Youtube
-                                 </span>
-                             </i>
-                         </FooterLink>
-                     </Column>
-                 </Row>
-             </Footer>
-        </Box>
-    );
-};
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <h3>Company Name</h3>
+          <p>About Us</p>
+          <p>Contact Us</p>
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+        </div>
+        <div className="footer-right">
+          <h3>Connect with Us</h3>
+          <ul className="social-icons">
+            <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+            <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+            <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 Astra. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
