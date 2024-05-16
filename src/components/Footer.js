d613b104734fd99bd90ee32802f1495626f654d1
astra <26215963+3tternp@users.noreply.github.com>
2024-05-16 12:19:02 +0545

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { HashLink } from 'react-router-hash-link';
+//import { HashLink } from 'react-router-hash-link';
 import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
@@ -8,7 +8,7 @@ import navIcon3 from '../assets/img/nav-icon3.svg';
 import './Footer.css'; // Import your CSS file for styling
 
 //export const Footer = () => {
-export const Footer = ({ activeLink, onUpdateActiveLink }) => {
+export const Footer = ({  }) => {
   return (
     <footer className="footer">
       <div className="footer-container">
