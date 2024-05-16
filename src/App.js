d63203498b02fdba6cc026fd5bc2ebad3ea92049
astra <26215963+3tternp@users.noreply.github.com>
2024-05-16 16:45:26 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Footer }  from "./components/Footer";
+import { Footer }  from "./components/Footer.js";
 
 function App() {
   return (
