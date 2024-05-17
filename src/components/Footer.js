ec8ee98453b3a3c740d166fd55841bb78752db3a
astra <26215963+3tternp@users.noreply.github.com>
2024-05-17 10:38:15 +0545

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,6 @@ export const Footer = () => {
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-left">
-          <h3>Astra</h3>
-        </div>
-        <div className="footer-right">
-                    <span className="navbar-text">
-              <div className="social-icon">
-                <a href="https://www.linkedin.com/in/prem-basnet524/"><img src={navIcon1} alt="" /></a>
-                <a href="https://www.facebook.com/profile.php?id=100051820911738"><img src={navIcon2} alt="" /></a>
-                <a href="https://www.instagram.com/astra.x3/"><img src={navIcon3} alt="" /></a>
-              </div>
-              <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
-              </HashLink>
-            </span>
           </ul>
         </div>
       </div>
