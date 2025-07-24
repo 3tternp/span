466342883f82a23111429db2c92cb63194a1aae0
Astra <26215963+3tternp@users.noreply.github.com>
2025-07-24 07:21:32 +0545

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import projImg4 from "../assets/img/project-img4.png";
 import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -40,7 +41,7 @@ export const Projects = () => {
     {
       title: "🔐 Incident Response",
       description: "Cybersecurity Incident Management & Recovery",
-      imgUrl: projImg3,
+      imgUrl: projImg6,
     },
   ];
 
